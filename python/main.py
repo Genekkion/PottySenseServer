a555a2425187048cc15e29fbf227aeadac953464
Genekkion <genekkion@live.com>
2024-02-04 13:23:47 +0800

    fixed telebot

--- python/main.py
+++ python/main.py
@@ -5,7 +5,7 @@ import telegram
 from telegram.ext import Application, CommandHandler, ContextTypes
 import sqlite3
 import functools
-
+from datetime import datetime
 
 def set_env():
     file = open("./.env", "r")
@@ -56,49 +56,26 @@ def authorized(func):
     return wrapper
 
 
-async def search_by_id(update: Update, context: ContextTypes.DEFAULT_TYPE, query: list[int]):
-    for id in query:
-        if id == 0:
-            continue
-        res = cursor.execute(
-            """SELECT first_name, last_name
-            FROM Clients
-            WHERE id = ?
-            """, (id, )).fetchall()
-        if len(res) == 0:
-            await update.message.reply_text(
-                text='Search for "'+str(id)+'" returned no results\.',
-                parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
-            continue
-        message = "*Search for id:* \[" + str(id) + "\]\n"
-        for entry in res:
-            message += "\[" + str(id) + "\] "
-            message += entry[1] + " " + entry[2] + "\n"
+async def search_by_name(update: Update, context: ContextTypes.DEFAULT_TYPE, name: str):
+    query_name = name + "%"
+    res = cursor.execute(
+        """SELECT id, first_name, last_name
+        FROM Clients
+        WHERE first_name LIKE ? COLLATE NOCASE
+        OR last_name LIKE ? COLLATE NOCASE
+        """, (query_name, query_name)).fetchall()
+    if len(res) == 0:
         await update.message.reply_text(
-            text=message, parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
+            text='Search for "'+name+'" returned no results\.',
+            parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
+        return
 
-
-async def search_by_name(update: Update, context: ContextTypes.DEFAULT_TYPE, query: list[str]):
-    for name in query:
-        query_name = name + "%"
-        res = cursor.execute(
-            """SELECT id, first_name, last_name
-            FROM Clients
-            WHERE first_name LIKE ? COLLATE NOCASE
-            OR last_name LIKE ? COLLATE NOCASE
-            """, (query_name, query_name)).fetchall()
-        if len(res) == 0:
-            await update.message.reply_text(
-                text='Search for "'+name+'" returned no results\.',
-                parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
-            continue
-
-        message = '*Search for name:* "' + name + '"\n'
-        for entry in res:
-            message += "\[" + str(entry[0]) + "\] "
-            message += entry[1] + " " + entry[2] + "\n"
-        await update.message.reply_text(
-            text=message, parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
+    message = '*Search for name:* "' + name + '"\n'
+    for entry in res:
+        message += "\[" + str(entry[0]) + "\] "
+        message += entry[1] + " " + entry[2] + "\n"
+    await update.message.reply_text(
+        text=message, parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
 
 
 def parse_int(s: str) -> int:
@@ -111,14 +88,8 @@ def parse_int(s: str) -> int:
 @ authorized
 async def search(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     args = (update.message.text.split(sep=" "))[1:]
-    if args[0].lower() == "id":
-        await search_by_id(update, context, [parse_int(s) for s in args[1:]])
-    elif args[0].lower() == "name":
-        await search_by_name(update, context, args[1:])
-    else:
-        await update.message.reply_text(
-            text="Incorrect arguments supplied\! \nE\.g\. /search name John Jane \nE\.g\. /search id 1 \n",
-            parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
+    for arg in args:
+        await search_by_name(update, context, arg)
 
 
 def parse_time(seconds: int) -> str:
@@ -154,18 +125,29 @@ async def get_client(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None
 async def current(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     username = update.message.from_user.username
     res = cursor.execute(
-        """SELECT Clients.id, Clients.first_name, Clients.last_name
+        """SELECT Clients.id, Clients.first_name, Clients.last_name, Clients.last_record
         FROM Clients INNER JOIN Watch
         ON Clients.id = Watch.client_id
         INNER JOIN TOfficers
         ON TOfficers.id = Watch.to_id
         WHERE TOfficers.telegram = ?
+        ORDER BY Clients.id
         """, (username,)).fetchall()
+    
+    current_time = datetime.now()
     message = "*Currently tracking*\n"
     for entry in res:
-        message += "[" + str(entry[0]) + "] "
-        message += entry[1] + " " + entry[2] + "\n"
-
+        message += "\[" + str(entry[0]) + "\] "
+        message += entry[1] + " " + entry[2] + " \- "
+        last_record = datetime.strptime(entry[3], "%Y-%m-%d %H:%M:%S")
+        time_elapsed = current_time - last_record
+        hours, remainder = divmod(time_elapsed.total_seconds(), 3600)
+        hours -= 8 # timezone diff
+        if hours < 0:
+            hours += 24
+        minutes = remainder // 60
+        time_elapsed = "{:02}".format(int(hours))+":"+"{:02}".format(int(minutes))
+        message += time_elapsed + "\n"
     await update.message.reply_text(
         text=message, parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
 
@@ -173,10 +155,10 @@ async def current(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 @ authorized
 async def all_clients(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     res = cursor.execute(
-        "SELECT id, first_name, last_name FROM Clients").fetchall()
+        "SELECT id, first_name, last_name FROM Clients ORDER BY id").fetchall()
     message = "*List of clients*\n"
     for entry in res:
-        message += "[" + str(entry[0]) + "] "
+        message += "\[" + str(entry[0]) + "\] "
         message += entry[1] + " " + entry[2] + "\n"
     await update.message.reply_text(
         text=message, parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
@@ -198,13 +180,60 @@ async def track_client(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
             continue
 
         res = cursor.execute(
+                """SELECT id FROM Clients
+                WHERE id = ?""",
+                (id,)).fetchone()
+        if res is None:
+            await update.message.reply_text(
+                    text="No client found with id: \["+str(id)+"\]\n",
+                    parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
+            continue
+
+        res = cursor.execute(
             """INSERT OR IGNORE
             INTO Watch (to_id, client_id)
             VALUES (?, ?)""",
             (to_id, id))
+        db.commit()
 
         await update.message.reply_text(
-            text="Tracking client \["+str(id)+"\]",
+                text="Tracking client \["+str(id)+"\]",
+                parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
+
+
+@ authorized
+async def untrack_client(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    args = [parse_int(s) for s in (update.message.text.split(sep=" "))[1:]]
+    username = update.message.from_user.username
+    res = cursor.execute(
+            """SELECT id FROM tofficers
+            WHERE telegram = ?""",
+            (username,)).fetchone()
+
+    to_id = res[0]
+
+    for id in args:
+        if id == 0:
+            continue
+
+        res = cursor.execute(
+                """SELECT id FROM Clients
+                WHERE id = ?""",
+                (id,)).fetchone()
+        if res is None:
+            await update.message.reply_text(
+                    text="No client found with id: \["+str(id)+"\]\n",
+                    parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
+            continue
+
+        res = cursor.execute(
+            """DELETE FROM Watch
+            WHERE to_id = ? AND
+            client_id = ?""",
+            (to_id, id))
+        db.commit()
+        await update.message.reply_text(
+            text="No longer tracking client \["+str(id)+"\]",
             parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
 
 
@@ -213,10 +242,26 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if (update is not None and update.message is not None and
             update.message.from_user is not None):
         await update.message.reply_text(
-            "Your account has been registered with PottySense! \nPlease return to the portal.")
+            "Your account has been registered with PottySense!")
     else:
         await update.message.reply_text(
             "Error registering your account with PottySense! \nPlease try again later.")
+
+
+'''
+@ authorized
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(
+        text="""*The following commands are supported:*
+    /start \- register your Telegram account
+    /current \- list currently tracked clients
+    /clients \- list all clients
+    /search \- search by id or by name
+    /id \- get information of a client
+    /track \- track a client
+    /untrack \- remove tracking of a client""",
+        parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
+'''
 
 
 def main() -> None:
@@ -229,6 +274,8 @@ def main() -> None:
     application.add_handler(CommandHandler("search", search))
     application.add_handler(CommandHandler("id", get_client))
     application.add_handler(CommandHandler("track", track_client))
+    application.add_handler(CommandHandler("untrack", untrack_client))
+    application.add_handler(CommandHandler("help", help))
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
