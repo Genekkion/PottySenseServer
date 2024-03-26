5e4fd69e246ad3296a20bcca6fd626d42ad5217d
Genekkion <genekkion@live.com>
2024-03-26 11:58:48 +0800

    stuff

--- db.sh
+++
@@ -1,1 +0,0 @@
-touch sqlite.db && rm sqlite.db && touch sqlite.db && sqlite3 sqlite.db < schema.sql
--- example.sql
+++ example.sql
@@ -1,10 +1,7 @@
 INSERT INTO Clients (first_name, last_name, gender)
-VALUES ('john', 'doe', 'male'),
-('jane', 'doe', 'female');
+VALUES ('John', 'Doe', 'male'),
+('Jane', 'Doe', 'female');
 
 INSERT INTO TOfficers (first_name, last_name,
-    username, password, telegram)
-VALUES ('oonga', 'boonga', 'oooo', 'asaSAcas', 'tele123');
-
-INSERT INTO Watch (to_id, client_id)
-VALUES (1, 1);
+    username, password)
+VALUES ('hello', 'world', 'hellow', 'world');
